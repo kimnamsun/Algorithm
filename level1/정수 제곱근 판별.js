83aaaf60d04bd3dd07fd8e81a118d396e0d938d7
kimnamsun <nsunny0908@naver.com>
2021-03-10 15:50:52 +0900

    0310 update

--- level1/정수 제곱근 판별.js
+++ level1/정수 제곱근 판별.js
@@ -12,13 +12,11 @@ function solution(n) {
     return answer;
 }
 
-function solution(n) {
-    const sqrt = Math.sqrt(n);
-    return Number.isInteger(sqrt) ? Math.pow(sqrt + 1, 2) : -1;
-}
-
 //방법2
 function solution(n) {
     const sqrt = Math.sqrt(n);
     return Number.isInteger(sqrt) ? (sqrt + 1) ** 2 : -1;
 }
+
+// def solution(n):
+//     return ((n**0.5)+1)**2 if((n**0.5)%1==0) else -1
--- level1/콜라츠 추측.js
+++ level1/콜라츠 추측.js
@@ -9,3 +9,16 @@ function solution(num) {
 
     return cnt >= 500 ? -1 : cnt;
 }
+
+//방법2
+function solution(num) {
+
+    for (let i = 0; i < 500; i++) {
+        if (num === 1) {
+            return i;
+        } else {
+            num = num % 2 === 0 ? num / 2 : (num * 3) + 1;
+        }
+    }
+    return -1;
+}
