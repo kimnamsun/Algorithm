3ed4b1a16d5c51f5b49d228a54d548a1b0cb26c9
kimnamsun <nsunny0908@naver.com>
2021-03-13 18:32:38 +0900

    0313 update

--- level1/완주하지 못한 선수.js
+++ level1/완주하지 못한 선수.js
@@ -1,3 +1,15 @@
+
+function solution(participant, completion) {
+    participant.sort();
+    completion.sort();
+    
+    for (let i in participant) {
+        if (participant[i] !== completion[i]) {
+            return participant[i]
+        }
+    }
+}
+//방법2
 function solution(participant, completion) {
     participant.sort();
     completion.sort();
@@ -5,16 +17,4 @@ function solution(participant, completion) {
     let answer = participant.filter((str, idx) => str !== completion[idx])
 
     return answer[0];
-}
-
-//방법2
-function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
-
-    for (let i in participant) {
-        if (participant[i] !== completion[i]) {
-            return participant[i]
-        }
-    }
 }
