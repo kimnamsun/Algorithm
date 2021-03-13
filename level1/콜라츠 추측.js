68e2dd1b2405063ff2ce87069ba5f2bc63eab192
kimnamsun <nsunny0908@naver.com>
2021-03-13 16:58:37 +0900

    0313 update

--- level1/콜라츠 추측.js
+++ level1/콜라츠 추측.js
@@ -1,12 +1,10 @@
 function solution(num) {
-
     let cnt = 0;
 
     while (num !== 1) {
         num = num % 2 === 0 ? num / 2 : (num * 3) + 1;
         cnt++;
     }
-
     return cnt >= 500 ? -1 : cnt;
 }
 
--- level1/행렬의 덧셈.js
+++ level1/행렬의 덧셈.js
@@ -28,8 +28,10 @@ function solution(arr1, arr2) {
 
   for (let i in arr1) {
     answer.push([]);
+    // [ [], [] ]
     for (let j in arr1[i]) {
       answer[i].push(arr1[i][j] + arr2[i][j])
+      // [ [1, 2], [3, 4] ]
     }
   }
   return answer;
