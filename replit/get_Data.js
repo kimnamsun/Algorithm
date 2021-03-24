3d35f022f4c28d64fe7c8d6c9c553d1e79438e8d
kimnamsun <nsunny0908@naver.com>
2021-03-24 15:50:28 +0900

    0324 update

---
+++ replit/get_Data.js
@@ -0,0 +1,16 @@
+function getData(salesArr, reviewArr, likeArr) {
+    function sumArr(arr) {
+        let sum = 0;
+        for (let i in arr) {
+            sum += arr[i][1];
+        }
+        return sum;
+    }
+
+    let obj = {
+        sumAmount: sumArr(salesArr),
+        sumReview: sumArr(reviewArr),
+        sumLike: sumArr(likeArr)
+    }
+    return obj;
+}
